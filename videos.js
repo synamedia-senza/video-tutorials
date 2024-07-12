c5a0a3c00f48cba093db0927f37b48b477acb271
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-07-12 18:50:25 -0400

    Updated video content.

--- videos.js
+++ videos.js
@@ -10,49 +10,49 @@ const videos = [
     "id": "debugger",
     "thumb": "thumbs/debugger.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/debugger/debugger.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Using the Remote Debugger",
+    "description": "This video tutorial shows how you can use the Remote Debugger to interact with your cloud connector device and debug your web app. You'll also learn how to use the debugger to run a sample app by changing the URL location."
   },
   {
     "id": "sample-app",
     "thumb": "thumbs/sample-app.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/sample-app/sample-app.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Running a Sample App",
+    "description": "This video tutorial shows how to write an app that plays two videos, one picture-in-picture and the other fullscreen, with animations to switch them back and forth."
   },
   {
     "id": "zoom",
     "thumb": "thumbs/zoom.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/zoom/zoom.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Picture in Picture",
+    "description": "This video tutorial shows how to write an app that plays two videos, one picture-in-picture and the other fullscreen, with animations to switch them back and forth."
   },
   {
     "id": "flags",
     "thumb": "thumbs/flags.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/flags/flags.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Geolocation",
+    "description": "This video tutorial shows how to geolocate the viewer’s cloud connector by getting its IP address from the Senza client library. Warning: contains terrible fake accents."
   },
   {
     "id": "speech-recognition",
     "thumb": "thumbs/speech-recognition.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/speech-recognition/speech-recognition.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Speech Recognition",
+    "description": "This video tutorial shows how to use a QR code to pair a mobile phone, capture voice input using WebKit speech recognition, send the input to the TV app using Socket.IO, and generate images using OpenAI."
   },
   {
     "id": "authentication",
     "thumb": "thumbs/authentication.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/authentication/authentication.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Device Authentication",
+    "description": "This video tutorial shows how to implement device-based authentication, so your users never need to think about usernames and passwords. Alternatively you can show a QR code that lets users sign in on a mobile phone the first time they use a device."
   },
   {
     "id": "remote",
     "thumb": "thumbs/remote.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/remote/remote.mpd",
-    "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "title": "Smart Remote",
+    "description": "This video tutorial shows how to make a smart remote control that runs on a mobile phone. It emulates the buttons on the standard Bluetooth remote, and adds a trackpad for gesture input, a keyboard for text input, and a microphone for voice input."
   },
 ];
