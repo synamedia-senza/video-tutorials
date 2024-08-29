a58a1e4932ea90f65fe75156b4a02e00248fe503
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-08-29 14:37:16 -0400

    Added new videos.

--- videos.js
+++ videos.js
@@ -1,24 +1,45 @@
 const videos = [
   {
+    "id": "developer-overview",
+    "thumb": "thumbs/developer-overview.jpg",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/developer-overview/developer-overview.mpd",
+    "title": "Developer Overview",
+    "description": "An overview of the Senza developer tools, including the Senza Console, Device Simulator, Remote Debugger and developer documentation."
+  },
+  {
+    "id": "senza-app",
+    "thumb": "thumbs/senza-app.jpg",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/senza-app/senza-app.mpd",
+    "title": "Senza App",
+    "description": "Explore what Senza can do by trying out the Senza App, featuring an informative slideshow, a sample TV experience, and some demo apps that demonstrate the advanced capabilities of the platform."
+  },
+  {
     "id": "simulator",
     "thumb": "thumbs/simulator.jpg",
-    "url": "https://senza-developer.s3.amazonaws.com/streams/simulator/simulator.mpd",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/simulator-v2/simulator-v2.mpd",
     "title": "Using the Device Simulator",
-    "description": "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device."
+    "description": "See how you can use the device simulator to preview how your app will run on a physical cloud connector device."
   },
   {
     "id": "debugger",
     "thumb": "thumbs/debugger.jpg",
-    "url": "https://senza-developer.s3.amazonaws.com/streams/debugger/debugger.mpd",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/debugger-v2/debugger-v2.mpd",
     "title": "Using the Remote Debugger",
-    "description": "This video tutorial shows how you can use the Remote Debugger to interact with your cloud connector device and debug your web app. You'll also learn how to use the debugger to run a sample app by changing the URL location."
+    "description": "Learn how you can use the Remote Debugger to interact with your cloud connector device and debug your web app. You'll also learn how to use the debugger to run a sample app by changing the URL location."
   },
   {
     "id": "sample-app",
     "thumb": "thumbs/sample-app.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/sample-app/sample-app.mpd",
     "title": "Running a Sample App",
-    "description": "This video tutorial shows how to write an app that plays two videos, one picture-in-picture and the other fullscreen, with animations to switch them back and forth."
+    "description": "Learn how to run a sample app that lets you switch back and forth between playing video in the web app and streaming video directly to the device. You'll also learn how to use ngrok to run an app on your computer in the simulator."
+  },
+  {
+    "id": "sample-apps-tour",
+    "thumb": "thumbs/sample-apps-tour.jpg",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/sample-apps-tour/sample-apps-tour.mpd",
+    "title": "Sample Apps Tour",
+    "description": "Come take a tour of all the sample code apps available on the Senza developer portal! We'll highlight each app in the documentation and give a quick demo of the app in the Simulator."
   },
   {
     "id": "zoom",
@@ -46,13 +67,20 @@ const videos = [
     "thumb": "thumbs/authentication.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/authentication/authentication.mpd",
     "title": "Device Authentication",
-    "description": "This video tutorial shows how to implement device-based authentication, so your users never need to think about usernames and passwords. Alternatively you can show a QR code that lets users sign in on a mobile phone the first time they use a device."
+    "description": "Learn how you can use device-based authentication, so your users never need to think about usernames and passwords. Alternatively you can show a QR code that lets users sign in on a mobile phone the first time they use a device."
   },
   {
     "id": "remote",
     "thumb": "thumbs/remote.jpg",
     "url": "https://senza-developer.s3.amazonaws.com/streams/remote/remote.mpd",
     "title": "Smart Remote",
-    "description": "This video tutorial shows how to make a smart remote control that runs on a mobile phone. It emulates the buttons on the standard Bluetooth remote, and adds a trackpad for gesture input, a keyboard for text input, and a microphone for voice input."
+    "description": "Learn how to make a smart remote control that runs on a mobile phone. It emulates the buttons on the standard Bluetooth remote, and adds a trackpad for gesture input, a keyboard for text input, and a microphone for voice input."
+  },
+  {
+    "id": "zombies",
+    "thumb": "thumbs/zombies.jpg",
+    "url": "https://senza-developer.s3.amazonaws.com/streams/zombies/zombies.mpd",
+    "title": "Emergency Alerts",
+    "description": "Learn how to send emergency alerts to Senza devices using the Group Messaging API, in the event of a zombie apocalypse or tsunami."
   },
 ];
